5318a5222a350b9185a2d9a43f525bbff4a80f6d
elias jimenez <bseliasjimenez14@gmail.com>
2023-07-19 20:21:18 -0300

    fix: modify nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,16 +2,10 @@
 
 import { useState } from "react";
 
-import { HiMenuAlt4 } from "react-icons/hi";
-import { AiOutlineClose } from "react-icons/ai";
-
-import NavbarItem from "./NavbarItem";
 import { useConnectWallet } from "@web3-onboard/react";
 
 const Navbar = () => {
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
-
-  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <nav className="flex w-full items-center justify-between p-4 md:justify-center animate-slide-in bg-gradient-to-r from-blue-400 to-purple-500">
@@ -20,7 +14,7 @@ const Navbar = () => {
           <h1 className="text-2xl font-bold text-white">CryptoBro</h1>
         </a>
       </div>
-      <ul className="hidden flex-initial list-none flex-row items-center justify-between text-white md:flex space-x-4">
+      <ul className="flex-initial list-none flex-row items-center justify-between text-white space-x-4">
         <button
           disabled={connecting}
           onClick={() => (wallet ? disconnect(wallet) : connect())}
@@ -29,41 +23,6 @@ const Navbar = () => {
           {connecting ? "Connecting" : wallet ? "Disconnect" : "Connect"}
         </button>
       </ul>
-      <div className="relative flex">
-        {!toggleMenu && (
-          <HiMenuAlt4
-            fontSize={28}
-            className="cursor-pointer text-white md:hidden"
-            onClick={() => setToggleMenu(true)}
-          />
-        )}
-        {toggleMenu && (
-          <AiOutlineClose
-            fontSize={28}
-            className="cursor-pointer text-white md:hidden"
-            onClick={() => setToggleMenu(false)}
-          />
-        )}
-        {toggleMenu && (
-          <ul
-            className="blue-glassmorphism fixed -top-0 -right-2 z-10 flex h-screen w-[70vw] animate-slide-in list-none
-            flex-col items-end justify-start rounded-md p-3 text-white shadow-2xl md:hidden"
-          >
-            <li className="my-2 w-full text-xl">
-              <AiOutlineClose onClick={() => setToggleMenu(false)} />
-            </li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => (
-                <NavbarItem
-                  key={item + index}
-                  title={item}
-                  classprops="my-2 text-lg"
-                />
-              )
-            )}
-          </ul>
-        )}
-      </div>
     </nav>
   );
 };
